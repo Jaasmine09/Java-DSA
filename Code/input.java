560515f74b7dbb0d04d2bf645dd25ce9782dfbc2
Jasmine G <127309284+Jaasmine09@users.noreply.github.com>
2023-04-10 15:52:26 +0530

    Add files via upload

---
+++ Code/input.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class input {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);//sc is object of scanner
+       // String input = sc.next(); //to print single word.
+        //System.out.println(input);
+
+       //String name = sc.nextLine();//to print two words.
+       //System.out.println(name);
+
+      // to print numbers
+      //int number = sc.nextInt();
+      //System.out.println(number);
+
+      //to print float number
+      float price = sc.nextFloat();
+      System.out.println(price);
+
+    }
+    
+}
---
+++ Code/variable.java
@@ -0,0 +1,15 @@
+public class variable{
+    public static void main(String args[]){
+        int a=10;
+        int b = 25;
+        System.out.println(a);
+        System.out.println(b);
+        //whatever printed in double quotes will be printed as it is, we just want to print the value of variables and b so we are not using quotes.
+        String name = "Jasmine";
+        System.out.println(name);
+
+        a = b;
+        System.out.println(b);
+        
+    }
+}
