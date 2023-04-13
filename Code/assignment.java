d8eb812d07420c8e9628a199b916c7fc4bc812f1
Jasmine G <127309284+Jaasmine09@users.noreply.github.com>
2023-04-13 09:44:08 +0530

    Add files via upload

---
+++ Code/assignment.java
@@ -0,0 +1,31 @@
+public class assignment {
+    public static void main(String args[]){
+       int e = 10;
+       int f = e;
+       System.out.println(f);
+
+
+        int A = 10;
+        //A = A+10;
+        A+=10;
+        System.out.println(A);
+
+
+        int B= 10;
+        //B = B -  10;
+        B-=10;
+        System.out.println(B);
+
+        int C= 10;
+        //C = C * 10;
+        C*=10;
+        System.out.println(C);
+
+        int D = 10;
+        //D = D / 10;
+        D/=10;
+        System.out.println(D);
+
+    }
+    
+}
